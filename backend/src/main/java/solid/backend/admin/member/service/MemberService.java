52eaa4b549c12e65cebe08b337ed949cd7438d87
w-jins <jineen@naver.com>
2025-06-12 16:28:40 +0900

    fix: 불필요한 Member 삭제

--- backend/src/main/java/solid/backend/admin/member/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package solid.backend.admin.member.service;
-import solid.backend.admin.member.dto.MemberDto;
-import java.util.List;
-public interface MemberService {
-
-    List<MemberDto> getMemberList();
-}
