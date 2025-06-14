d51ccf41a7f554877f8987e29947ba1d650989e3
w-jins <jineen@naver.com>
2025-06-14 09:56:48 +0900

    refactor: 코드 리펙토링

--- backend/src/main/java/solid/backend/admin/orders/service/OrdersServiceImpl.java
+++ backend/src/main/java/solid/backend/admin/orders/service/OrdersServiceImpl.java
@@ -33,31 +33,18 @@ public class OrdersServiceImpl implements OrdersService {
     @Override
     public List<OrdersManagementDto> findSearchOrdersList(OrdersSearchDto request) {
         Map<String, String> map = new HashMap<>();
-        switch (request.getNumber()) {
-            case 1:
-                map.put("orderId", request.getData());
-                break;
-            case 2:
-                map.put("orderDt", request.getData());
-                break;
-            case 3:
-                map.put("orderState", request.getData());
-                break;
-            case 4:
-                map.put("memberId", request.getData());
-                break;
-            case 5:
-                map.put("productName", request.getData());
-                break;
-            case 6:
-                // 수량이 필요할까??, 어떤 수량 ..? 흠 ...
-                map.put("", request.getData());
-                break;
-            default:
-                // 이것도... 여행상품 고유 id 가 아닌 상품 이름이 필요할까 ..?
-                map.put("travelId", request.getData());
-                break;
-        }
+
+        String result = switch (request.getNumber()) {
+            case 1 -> "orderId";
+            case 2 -> "orderDt";
+            case 3 -> "orderState";
+            case 4 -> "memberId";
+            case 5 -> "productName";
+            case 6 -> "";
+            default -> "travelId";
+        };
+        map.put(result, request.getData());
+
         return ordersQueryRepository.findSearchOrdersList(map);
     }
 
