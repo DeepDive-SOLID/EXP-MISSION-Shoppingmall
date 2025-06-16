18eb1b179cfea6262f04383ce715b6d15fcb13f6
lnylnylnylny <lnylnylnylny@naver.com>
2025-06-17 03:26:58 +0900

    feat: 검색화면 캘린더 사이드바 추가

--- frontend/src/components/search/SearchSidebar/SearchSidebar.tsx
+++ frontend/src/components/search/SearchSidebar/SearchSidebar.tsx
@@ -1,7 +1,13 @@
 import styles from "./SearchSidebar.module.scss";
 import { korea_map } from "../../../assets";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/locale";
 
 const SearchSidebar = () => {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
   return (
     <div className={styles.sidebar}>
       <div className={styles.mapSection}>
@@ -9,9 +15,21 @@ const SearchSidebar = () => {
         <button className={styles.mapButton}>지도보기</button>
       </div>
 
-      <div className={styles.section}>
-        <h3>일정 선택</h3>
-        <input type="date" className={styles.dateInput} />
+      <div className={styles.calendarSection}>
+        <h2 className={styles.sectionTitle}>일정 선택</h2>
+        <DatePicker
+          selected={startDate}
+          onChange={dates => {
+            const [start, end] = dates as [Date, Date];
+            setStartDate(start);
+            setEndDate(end);
+          }}
+          startDate={startDate}
+          endDate={endDate}
+          selectsRange
+          inline
+          locale={ko}
+        />
       </div>
 
       <div className={styles.section}>
