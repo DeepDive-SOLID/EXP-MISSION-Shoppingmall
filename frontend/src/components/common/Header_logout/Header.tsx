773a619b50898a68a76b1d9b296e3511e648243f
lnylnylnylny <lnylnylnylny@naver.com>
2025-06-16 20:48:42 +0900

    feat: 로그아웃 헤더 추가

--- frontend/src/components/common/Header_logout/Header.tsx
+++ frontend/src/components/common/Header_logout/Header.tsx
@@ -1,5 +1,6 @@
 import styles from "./Header.module.scss";
-import { logo, logout } from "../../../assets/index";
+import { logo } from "../../../assets/index";
+import { IoIosSearch } from "react-icons/io";
 
 const Header = () => {
   return (
@@ -8,9 +9,24 @@ const Header = () => {
         <div className={styles.left}>
           <img src={logo} alt="Logo" className={styles.logo} />
         </div>
+
+        <div className={styles.center}>
+          <input
+            type="text"
+            placeholder="검색어를 입력하세요"
+            className={styles.searchInput}
+          />
+          <IoIosSearch />
+        </div>
+
         <div className={styles.right}>
-          <img src={logout} alt="Logout" className={styles.logoutIcon} />
-          <p className={styles.logoutText}>로그아웃</p>
+          <div className={styles.menu}>
+            <p className={styles.menuText}>로그인</p>
+          </div>
+
+          <div className={styles.menu}>
+            <p className={styles.menuText}>회원가입</p>
+          </div>
         </div>
       </header>
     </div>
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import Header from "../../components/common/Header_login/Header";
+import Header from "../../components/common/Header_logout/Header";
 
 const Home = () => {
   return (
