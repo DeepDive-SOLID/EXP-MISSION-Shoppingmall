6bc369e8a550df34fda4ca350e3e6766c03fa13d
lnylnylnylny <lnylnylnylny@naver.com>
2025-06-17 15:33:08 +0900

    feat: 검색화면 검색 결과 페이지 제작

---
+++ frontend/src/components/search/SearchMain/SearchMain.tsx
@@ -0,0 +1,56 @@
+import styles from "./SearchMain.module.scss";
+import { home_banner2 } from "../../../assets/";
+import { FaRegStar } from "react-icons/fa6";
+import { FaComments } from "react-icons/fa";
+
+const mockSearchResults = Array.from({ length: 4 }, (_, i) => ({
+  id: i,
+  title: "[부산] 먹방투어",
+  badges: ["# 먹방", "# 부산", "# 투어"],
+  date: "1박 2일 (2025.06.02 ~ 2025.06.03)",
+  rating: 4.8,
+  comments: 12,
+  price: "150,000원",
+}));
+
+const SearchMain = () => {
+  return (
+    <div className={styles.mainContent}>
+      <h2>"검색어" 검색 결과</h2>
+
+      {mockSearchResults.map(item => (
+        <div className={styles.searchItem} key={item.id}>
+          <div className={styles.leftSection}>
+            <img
+              src={home_banner2}
+              alt="검색 배너"
+              className={styles.listImg}
+            />
+          </div>
+          <div className={styles.rightSection}>
+            <div className={styles.listHeader}>
+              <p className={styles.listTitle}>{item.title}</p>
+              <div className={styles.listBadge}>
+                {item.badges.map((badge, idx) => (
+                  <span className={styles.badge} key={idx}>
+                    {badge}
+                  </span>
+                ))}
+              </div>
+              <p className={styles.listDate}>{item.date}</p>
+            </div>
+            <div className={styles.listContent}>
+              <FaRegStar className={styles.starIcon} />
+              <span className={styles.starRating}>{item.rating}</span>
+              <FaComments className={styles.commentIcon} />
+              <span className={styles.commentCount}>{item.comments}</span>
+              <span className={styles.price}>{item.price}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchMain;
--- frontend/src/pages/search/Search.tsx
+++ frontend/src/pages/search/Search.tsx
@@ -1,11 +1,16 @@
 import SearchSidebar from "../../components/search/SearchSidebar/SearchSidebar";
 import Header from "../../components/common/Header_login/Header";
+import SearchMain from "../../components/search/SearchMain/SearchMain";
+import styles from "./Search.module.scss";
 
 const Search = () => {
   return (
-    <div>
+    <div className={styles.pageWrapper}>
       <Header />
-      <SearchSidebar />
+      <div className={styles.contentWrapper}>
+        <SearchSidebar />
+        <SearchMain />
+      </div>
     </div>
   );
 };
