2d813d86359025eca233a3fcfe1e46450a2e9825
shinyyseon <epdlwl515@naver.com>
2025-06-21 15:14:19 +0900

    fix: 여행 상품 등록, 수정 버그

--- backend/src/main/java/solid/backend/admin/travel/dto/TravelUpdDto.java
+++ backend/src/main/java/solid/backend/admin/travel/dto/TravelUpdDto.java
@@ -2,11 +2,13 @@ package solid.backend.admin.travel.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TravelUpdDto {
     private Integer travelId;
     private String travelName;
