350f8b2a399cad67331ca11c7e2e205cb69a37d4
lnylnylnylny <lnylnylnylny@naver.com>
2025-06-25 03:06:03 +0900

    feat: 결제 실패 화면 제작

--- frontend/src/pages/payresult/PayFail.tsx
+++ frontend/src/pages/payresult/PayFail.tsx
@@ -1,11 +1,32 @@
 import Header from "../../components/common/Header_login/Header";
+import { FaTimesCircle } from "react-icons/fa";
 import styles from "./payResult.module.scss";
+import { home_banner } from "../../assets";
 
 const PayFail = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header />
+      <img src={home_banner} alt="배경 이미지" className={styles.bg} />
+      <div className={styles.resultBox}>
+        <FaTimesCircle className={styles.failIcon} />
+        <p className={styles.username}>
+          <span>이나영</span>
+          님의
+        </p>
+
+        <p className={styles.resultFailText}>결제를 실패하였습니다.</p>
+
+        <div className={styles.failMessage}>
+          잔액이 부족합니다.
+          <br />
+          다른 결제 수단을 이용해 주세요.
+        </div>
+
+        <button className={styles.retryButton}>결제하러 가기</button>
+      </div>
     </div>
   );
 };
+
 export default PayFail;
