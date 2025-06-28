9f48149c20bf10494880ab3860459d27f033612a
dyoni2 <delay1820@gachon.ac.kr>
2025-06-29 00:56:40 +0900

    refactor: 리팩토링 진행

--- frontend/src/api/mypage/cardApi.ts
+++ frontend/src/api/mypage/cardApi.ts
@@ -1,40 +1,61 @@
 import api from "../axios";
+import {
+  AddCardDto,
+  AddCardResponse,
+  GetCardImageResponse,
+  GetPaymentListResponse,
+  DeleteCardResponse,
+} from "../../types/mypage/card";
 
 // 카드 추가 API
-export const addCard = async (paymentDto: {
-  memberId: string;
-  paymentName: string;
-  paymentNum: number;
-  paymentEndDt: string;
-  paymentOwner: string;
-  paymentSecurity: number;
-  paymentPw: number;
-}) => {
-  const response = await api.post("/mypage/payment/addPaymentDto", paymentDto);
+export const addCard = async (
+  paymentDto: AddCardDto,
+): Promise<AddCardResponse> => {
+  const response = await api.post<AddCardResponse>(
+    "/mypage/payment/addPaymentDto",
+    paymentDto,
+  );
   return response.data;
 };
 
 // 카드 이미지 가져오기 API
-export const getCardImage = async (cardNumber: string) => {
-  const response = await api.post("/mypage/payment/getCardImg", cardNumber, {
-    headers: { "Content-Type": "application/json" },
-  });
+export const getCardImage = async (
+  cardNumber: string,
+): Promise<GetCardImageResponse> => {
+  const response = await api.post<GetCardImageResponse>(
+    "/mypage/payment/getCardImg",
+    cardNumber,
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+  );
   return response.data;
 };
 
 // 카드 리스트 가져오기 API
-export const getPaymentList = async (memberId: string) => {
-  const response = await api.post("/mypage/payment/getPaymentList", memberId, {
-    headers: { "Content-Type": "text/plain" },
-  });
+export const getPaymentList = async (
+  memberId: string,
+): Promise<GetPaymentListResponse> => {
+  const response = await api.post<GetPaymentListResponse>(
+    "/mypage/payment/getPaymentList",
+    memberId,
+    {
+      headers: { "Content-Type": "text/plain" },
+    },
+  );
   return response.data;
 };
 
 // 카드 삭제 API
-export const deleteCard = async (paymentId: number) => {
-  const response = await api.delete("/mypage/payment/deletePaymentDto", {
-    data: paymentId,
-    headers: { "Content-Type": "application/json" },
-  });
+export const deleteCard = async (
+  paymentId: number,
+): Promise<DeleteCardResponse> => {
+  const response = await api.delete<DeleteCardResponse>(
+    "/mypage/payment/deletePaymentDto",
+    {
+      data: paymentId,
+      headers: { "Content-Type": "application/json" },
+    },
+  );
   return response.data;
 };
