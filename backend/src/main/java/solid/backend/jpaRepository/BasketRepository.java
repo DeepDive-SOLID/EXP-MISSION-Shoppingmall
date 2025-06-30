527471645987e5fbc1e128e2f621c5c4a49827af
w-jins <jineen@naver.com>
2025-07-01 04:10:22 +0900

    fix: 매개변수 변경

--- backend/src/main/java/solid/backend/jpaRepository/BasketRepository.java
+++ backend/src/main/java/solid/backend/jpaRepository/BasketRepository.java
@@ -2,8 +2,10 @@ package solid.backend.jpaRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import solid.backend.entity.Basket;
+import solid.backend.entity.Member;
 import solid.backend.entity.Travel;
 
 public interface BasketRepository extends JpaRepository<Basket, Integer> {
-    void deleteAllByTravel(Travel travel);
+
+    void deleteAllByTravelAndMember(Travel travel, Member member);
 }
