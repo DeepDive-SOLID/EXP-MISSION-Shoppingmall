349b0d1bbdba0bf3962bf618a26a75c12f368854
w-jins <jineen@naver.com>
2025-07-01 03:27:15 +0900

    feat: 삭제 메서드 생성

--- backend/src/main/java/solid/backend/jpaRepository/BasketRepository.java
+++ backend/src/main/java/solid/backend/jpaRepository/BasketRepository.java
@@ -2,6 +2,8 @@ package solid.backend.jpaRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import solid.backend.entity.Basket;
+import solid.backend.entity.Travel;
 
 public interface BasketRepository extends JpaRepository<Basket, Integer> {
+    void deleteAllByTravel(Travel travel);
 }
